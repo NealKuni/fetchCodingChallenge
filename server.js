62fc8ae241bdd2ad8330c900daa055725ba8b546
nealichinohe <neal.kuni@yahoo.com>
2022-03-04 16:07:48 -0700

    added logic for spend transaction

--- server.js
+++ server.js
@@ -3,44 +3,44 @@ const express = require("express");
 const app = express();
 // instantiating a port number to use when sending http requests.
 const port = 8000;
-
 // added middelware functions to access and parse through post data
 app.use( express.json() );
 app.use( express.urlencoded({ extended: true }) );
 
-let userPoints = {
-    points = 0
-};
 
 const transactions = [];
 
 app.post( "/api/add", (req, res) => {
     transactions.push(req.body)
-        .then( () =>{
-            console.log(req.body);
-            res.json(req.body)
-        })
-        .catch((err)=>{
-            console.log(err);
-            res.status(400).json(err);
-        })
+    .then( () =>{
+        console.log(req.body);
+        res.json(req.body)
+    })
+    .catch((err)=>{
+        console.log(err);
+        res.status(400).json(err);
+    })
     
 });
 
 app.post( "/api/spend", (req, res) => {
     // add amount of points that will be spent to userPoints.
+    let userPoints = 100
     userPoints += req.body.points;
     // take the amount of points spent and subtract from the transactions array.
     let sortedTransactions = transactions.sort(function(a, b)  {
-        return new Date(a.timestamp) - new Date (b.timestamp);
+        return new Date(b.timestamp) - new Date (a.timestamp);
     });
     //loop through the sorted array and push elements into new array until spent points amount
     for ( let i = 0; i > sortedTransactions.length - 1; i++){
-        if (userPoints > 0){
+        if (userPoints > sortedTransactions[i].points){
         userPoints -= sortedTransactions[i].points;
+        sortedTransactions[i].points = 0;
         }
-        if (userPoints < 0) {
-        
+        if (userPoints < sortedTransactions[i].points) {
+        sortedTransactions[i].points -= userPoints;
+        userPoints = 0;
+        break
         }
     }
 
